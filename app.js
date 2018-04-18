aa2de7173922021ae0b591dfaa4b4795b9d2d62f
Ryanliszewski <ryanliszewski@gmail.com>
2018-04-18 14:43:07 -0700

    Changed ipn to remote

--- app.js
+++ app.js
@@ -41,7 +41,6 @@ let middleware = [
   CoinPayments.ipn({
     'merchantId': '5d47493d32482aef464ac46d11ccd4ef714bac28ff06726208bdcc487b5c5b16',
     'merchantSecret': '622c3064DBd9Fffd20B63655c0Ede039c0caF3E3eBc3601F4CDBFe6ee00e0774',
-     autoIpn: true
   }), 
   function (req, res, next) {
     // Handle via middleware
--- paymentClient.js
+++ paymentClient.js
@@ -2,25 +2,26 @@ var options = [];
 
 options.key = '5d47493d32482aef464ac46d11ccd4ef714bac28ff06726208bdcc487b5c5b16';
 options.secret = '622c3064DBd9Fffd20B63655c0Ede039c0caF3E3eBc3601F4CDBFe6ee00e0774';
-options.autoIpn = true; 
 
 var Coinpayments = require('coinpayments');
 var paymentClient = new Coinpayments(options);
 
 ipnCallback = () => {
-  paymentClient.on('autoipn', function(data){
-    const transactionID = Object.keys(data);
-    const transaction = data[transactionID];
-
-    console.log(transaction.status)
-      if(transaction.status == 1){
-        //Successful
-         
-       } else if (transaction.status == -1){
-         //Failure
-
-       } 
-    }); 
+  CoinPayments.on('ipn_fail', function(data){
+      // Handle failed transaction
+      console.log("IPN FAIL");
+      console.log(data);
+  });
+  CoinPayments.on('ipn_pending', function(data){
+      // Handle pending payment
+      console.log("IPN PENDING");
+      console.log(data);
+  });
+  CoinPayments.on('ipn_complete', function(data){
+      // Handle completed payment
+      console.log("IPN COMPLETE");
+      console.log(data);
+  });
 }
 
 module.exports = {
